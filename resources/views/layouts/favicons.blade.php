ae0464674a4ed9d2d8b72340d46cf9b0e51c764b
Manuel Strebel <manuel_st@msn.com>
2016-07-07 13:20:06 +0200

    update favicons

--- resources/views/layouts/favicons.blade.php
+++ resources/views/layouts/favicons.blade.php
@@ -1,7 +1,7 @@
-
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-<link rel="manifest" href="/manifest.json">
-<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=oLLxG9q6Gr">
+<link rel="icon" type="image/png" href="/favicon-32x32.png?v=oLLxG9q6Gr" sizes="32x32">
+<link rel="icon" type="image/png" href="/favicon-16x16.png?v=oLLxG9q6Gr" sizes="16x16">
+<link rel="manifest" href="/manifest.json?v=oLLxG9q6Gr">
+<link rel="mask-icon" href="/safari-pinned-tab.svg?v=oLLxG9q6Gr" color="#3498db">
+<link rel="shortcut icon" href="/favicon.ico?v=oLLxG9q6Gr">
 <meta name="theme-color" content="#b39292">
