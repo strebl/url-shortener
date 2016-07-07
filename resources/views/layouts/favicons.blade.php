7185a905da9459bf82e15185dcc66ab3e131838e
Manuel Strebel <manuel_st@msn.com>
2016-07-07 11:58:16 +0200

    [ci skip] add more favicon code

--- resources/views/layouts/favicons.blade.php
+++ resources/views/layouts/favicons.blade.php
@@ -18,3 +18,11 @@
 <meta name="msapplication-square150x150logo" content="mstile-150x150.png" />
 <meta name="msapplication-wide310x150logo" content="mstile-310x150.png" />
 <meta name="msapplication-square310x310logo" content="mstile-310x310.png" />
+
+
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="/manifest.json">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="theme-color" content="#b39292">
